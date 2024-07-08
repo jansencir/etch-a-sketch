84e4258dc8f7a9b642d359f94a8f3beea5ce2cf4
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-08 01:19:19 -1000

    Create functions to add default grid
    Creates a 16x16 grid by default with flex

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const container = document.querySelector(".container");
 
-const para = document.createElement("p");
-para.textContent = "Paragraph"
-container.appendChild(para);
+function createGrid(grid) {
+    if (grid === undefined) {
+        grid = 16;
+    }
+    
+    for (y = 0; y < grid; y++) {
+        const column = document.createElement("div");
+        column.classList.add("grid-column");
+        container.appendChild(column);
+        
+        for (x = 0; x < grid; x++) {
+            const row = document.createElement("div");
+            row.classList.add("grid-row");
+            column.appendChild(row);
+        }
+    }
+};
 
+createGrid();
